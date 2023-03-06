26bfbd80cdfc1574ba34f6a7a1a57db32daf9e72
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-06 18:57:01 +0530

    Generating Short ID and Storing in Schema

---
+++ Controllers/URL.js
@@ -0,0 +1,32 @@
+const { nanoid } = require("nanoid")
+const URL = require('../Models/URL')
+
+async function handleGenerateNewShortURL(request , response) {
+    // Getting the short ID for the given URL (Comes after '<link>/__')
+    const shortID = nanoid(8);
+    
+    // Getting Information from the request:
+    const body = request.body;
+
+    // In a case where the user doesn't provide the URL 
+    if(!body.URL){
+        response.status(400).json({
+            "Error": "Please provide the URL"
+        })
+    }
+
+    // Adding Information to the Database
+    await URL.create({
+        shortID: shortID,
+        redirectURL: body.URL,
+        visitHistory: []
+    })
+
+    return response.json({
+        shortID: shortID
+    })
+}
+
+module.exports = {
+    handleGenerateNewShortURL
+}
