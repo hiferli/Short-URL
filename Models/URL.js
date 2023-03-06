00b883505dce8baa888f73e4353174fee28b0306
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-06 18:40:12 +0530

    Creating Database Schema

---
+++ Models/URL.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const URLSchema = new mongoose.Schema({
+    // New Shortened URL
+    shortURL: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    // Original URL
+    redirectURL: {
+        type: String,
+        required: true
+    },
+
+    // Click History
+    visitHistory: [
+        {
+            // A collection of the number of times link is clicked
+            timestamp : {
+                type: Number
+            }
+        }
+    ], 
+} , 
+// Stores Timestamp of every entry
+{timestamps: true})
+
+const URL = mongoose.model("URL" , URLSchema);
+module.exports = URL;
