c8c5c0d373e2f47cd63dda581c89f085af570091
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-06 20:07:57 +0530

    Shifting to Cloud DB and Hiding Password

--- Index.js
+++ Index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 
+// DOTENV File
+require('dotenv').config({path: __dirname + '/.env'});
+
 // Connection to MongoDB
 const { connectToMongoDB } = require('./DatabaseConnection')
-connectToMongoDB("mongodb://localhost:27017/Short-URL").then(console.log(`Connection with MongoDB Established`)).catch((e) => console.log("Connection Error: " + e));
+connectToMongoDB(`mongodb+srv://Admin_Ishaan:${process.env.MONGODB_PASSWORD}@cluster0.s5eiovj.mongodb.net/?retryWrites=true&w=majority`).then(console.log(`Connection with MongoDB Established`)).catch((e) => console.log("Connection Error: " + e));
+
+app.use(express.json())
 
 // Importing the Route(s)
 const URLRoute = require("./Routes/URL")
