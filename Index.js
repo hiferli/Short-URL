ef116dc3151580e9af54f2ddd9500befa1d232ee
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-06 19:16:32 +0530

    Using URL Route in Index.js

--- Index.js
+++ Index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 
+// Importing the Route(s)
+const URLRoute = require("./Routes/URL")
+
+app.use('/URL' , URLRoute)
+
 app.listen(port , () => {
     console.log(`App running on PORT ${port}`)
 })
