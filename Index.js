05f0ea78abe8922fd8da132939f24dbbadedf820
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-07 14:49:00 +0530

    Rendering Without View Engines

--- Index.js
+++ Index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8000;
 const URL = require('./Models/URL')
 
 // DOTENV File
-require('dotenv').config({path: __dirname + '/.env'});
+require('dotenv').config({ path: __dirname + '/.env' });
 
 // Connection to MongoDB
 const { connectToMongoDB } = require('./DatabaseConnection')
@@ -18,16 +18,40 @@ app.use(express.json())
 const URLRoute = require("./Routes/URL")
 
 // Route for Converting to Short URL
-app.use('/URL' , URLRoute)
+app.use('/URL', URLRoute)
 
-app.get("/test" , (request , response) => {
-    response.end("<h1>Welcome to the testing sight of the app</h1>")
+app.get("/test", async (request, response) => {
+    const allURLs = await URL.find({});
+    console.log(allURLs);
+    return response.end(`
+    <!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+      </head>
+      <body>
+        <h1>Hello, world!</h1>
+        
+        <ul class="list-group">
+            ${allURLs.map((abc) => {
+                `<li> ${abc.shortID} </li>`
+            }).join('')}
+        </ul>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+      </body>
+    </html>
+    `)
 })
+
 // Extracting Original Website from ShortID 
 // And thereafter updating the clicks option
-app.get("/:shortID" , async (request , response) => {
+app.get("/url/:shortID" , async (request , response) => {
     const shortID = request.params.shortID;
-    
+
     const entry = await URL.findOneAndUpdate({
         // Thing needed to find
         shortID
@@ -44,6 +68,6 @@ app.get("/:shortID" , async (request , response) => {
     response.redirect(entry.redirectURL)
 })
 
-app.listen(port , () => {
+app.listen(port, () => {
     console.log(`App running on PORT ${port}`)
 })
