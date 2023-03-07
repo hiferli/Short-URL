392514ae7def5535f67a1d71d49fece012bd595b
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-07 15:07:39 +0530

    Static Routing

--- Routes/StaticRouter.js
+++ Routes/StaticRouter.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/" , (request , response) => {
+    return response.render("Home")
+})
 
 module.exports = router;
