ab99e4b537e17c465ee8a629318c5e9ca6ab3ab7
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-03-07 14:53:47 +0530

    Using Server Redirection with EJS

--- Index.js
+++ Index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
+const path = require('path');
 
 // Getting Models
 const URL = require('./Models/URL')
@@ -15,6 +16,9 @@ connectToMongoDB(`mongodb+srv://Admin_Ishaan:${process.env.MONGODB_PASSWORD}@clu
 // Setting up EJS as View Engine
 app.set("view engine" , "ejs")
 
+// Using Directory with all EJS Files
+app.set("views" , path.resolve("./Views"));
+
 // Allowing CORS Sharing
 app.use(express.json())
 
@@ -25,30 +29,7 @@ const URLRoute = require("./Routes/URL")
 app.use('/URL', URLRoute)
 
 app.get("/test", async (request, response) => {
-    const allURLs = await URL.find({});
-    console.log(allURLs);
-    return response.end(`
-    <!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap demo</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-      </head>
-      <body>
-        <h1>Hello, world!</h1>
-        
-        <ul class="list-group">
-            ${allURLs.map((abc) => {
-                `<li> ${abc.shortID} </li>`
-            }).join('')}
-        </ul>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-      </body>
-    </html>
-    `)
+    return response.render('Home');
 })
 
 // Extracting Original Website from ShortID 
